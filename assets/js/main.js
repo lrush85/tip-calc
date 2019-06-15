ba501d17f63d3b1de4e8ade6327760f99f0f37a5
lrush85 <lance.rush1@gmail.com>
2019-06-14 19:56:00 -0700

    Added some more styling and made the number of people adjust the out look.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,26 +7,30 @@ function calTotal() {
 
         let percent = document.getElementById('percent').value;
         percent = parseFloat(percent) / 100;
-
+        let percentAmount = amount * percent;
         let total = (amount * percent) + amount;
 
         let people = document.getElementById('people').value;
         people = parseFloat(people);
 
-        if (people > 0) {
-            total = total / people;
-            document.getElementById('totalAmount').textContent = total.toFixed(2);
+        document.getElementById('bill').textContent = 'Bill: $' + amount.toFixed(2);
+        document.getElementById('tip').textContent = 'Tip: $' + percentAmount.toFixed(2);
+
+        if (people > 1) {
+            let totalPerPerson = total / people;
+            document.getElementById('split').textContent = 'Split: $' + people.toFixed(2);
+            document.getElementById('split').textContent = 'Total: $' + totalPerPerson.toFixed(2) + ' per person';
         }
 
-        if (isNaN) {
-            document.getElementById('totalAmount').className = 'hidden';
-        }
+        document.getElementById('totalAmount').textContent = 'Total: $' + total.toFixed(2);
 
-        document.getElementById('totalAmount').textContent = total.toFixed(2);
+        document.getElementById('clear').addEventListener('click', function(event) {
+            event.preventDefault();
 
-        trimVal('amount');
-        trimVal('percent');
-        trimVal('people');
+            trimVal('amount');
+            trimVal('percent');
+            trimVal('people');
+        });
 
     });
 
